ff643dff033ff9e5d79ff1ccfe53b7366cd3ea58
celeborne <coachluke@gmail.com>
2020-04-08 12:50:37 -0400

    studying for logic

---
+++ infofor/forloop1.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+#create the list called vendors
+vendors= ["cisco", "juniper", "big_ip", "f5", "arista"]
+#loop across the list of vendors
+for x in vendors:
+    print("The vendor is: " + x)
+print("\nOur loop has ended.")
