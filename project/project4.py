2e8237b6296a5b07f539ea8478bd3f2785d2ab06
celeborne <coachluke@gmail.com>
2020-04-17 21:54:50 -0400

    1854 PM friday week 2

---
+++ project/project4.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python5
+from random import randint
+import dice
+import sys
+import time
+
+print(
+"""
+On the High Seas
+A Choose Your Own Adventure Python Script
+Written by Luke Thompson
+""")
+time.sleep(1)
+
+def intro():
+    intro = open("try.txt", "r")
+    print(intro.readlines()[0])
+intro()
+
+def faction():
+    while True:
+        factionpopout = open("try.txt", "r")
+        print(
+        """
+        Choose a faction to view, or press \'q\' to exit.
+        [1]: Royal Irish Navy
+        [2]: Blackfire Consortium
+        """)
+        try:
+            playerfaction = input(" > ")
+            if playerfaction.strip() == "1":
+                print(factionpopout.readlines()[1])
+                time.sleep(1)
+                print("Are you happy with this selection? Once you confirm, you cannot go back!")
+                confirmfaction = input("Select [1] > to confirm or [2] > to start over > ")
+                if confirmfaction.strip() == "1":
+                    playerclass() #calls playerclass function to set player class 
+                elif confirmfaction.strip() == "2":
+                    faction() #calls restart on faction
+                elif confirmfaction.lower().strip() == "q":
+                    sys.exit()
+            elif playerfaction.strip() == "2":
+                print(factionpopout.readlines()[6])
+                time.sleep(1)
+                print("Are you happy with this selection? Once you confirm, you cannot go back!")
+                confirmfaction = input("Select [1] > to confirm or [2] > to start over > ")
+                if confirmfaction.strip() == "1":
+                    playerclass() #calls playerclass function to set player class
+                elif confirmfaction.strip() == "2":
+                    faction() #calls restart on faction() function
+                elif confirmfaction.lower().strip() == "q":
+                    sys.exit()
+            elif playerfaction.lower().strip() == "q":
+                sys.exit()
+        except:
+                print("Please make a valid selection: 1, 2, or \'q\' for quit.")
+faction()
+
+def playerclass():
+    classpopout = open("try.txt", "r")
+    print(
+    """
+    Choose a player class, or press \'q\' to exit.
+    [1]: Brawler
+    [2]: Rogue
+    [3]: Mage
+    [4]: Ranger
+    [5]: Paladin
+    """)
+    playerclass = input(" > ")
+
